5fd4d09ddec49dd6757b05516fe005c27bb72297
Despo Pentara <despo@despo.me>
2013-12-20 21:21:10 +0000

    remove logging

--- public/js/application.js
+++ public/js/application.js
@@ -4,8 +4,6 @@ $( document ).ready(function() {
     if (selected != null) {
 
       while (selected.height() > $(this).height()+100) {
-        console.log($(this).height());
-        console.log(selected.height());
         article = selected.children('article').first().remove();
         $(this).append(article);
       };
