2de103bec2348a083ea8c4b17bff44cd6c376650
Despo Pentara <despo@despo.me>
2013-12-24 17:57:17 +0000

    ensure markdown gets rendered correctly

--- lib/kobol/presenters/issue.rb
+++ lib/kobol/presenters/issue.rb
@@ -8,7 +8,7 @@ module Kobol::Presenters
     end
 
     def body
-      @body ? Markdown.new(@body).to_html : nil
+      @body ? Markdown.new(@body, :smart, :filter_html).to_html : nil
     end
 
     def repo
